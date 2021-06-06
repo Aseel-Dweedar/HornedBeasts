088e83ee27c9afaabc620bf495a1321e3cce6566
Aseel-Dweedar <archaseel.1992@gmail.com>
2021-06-07 00:35:10 +0300

    finish lab1 /301

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Footer extends React.Component {
+    render() {
+        const authorName = 'ASEEL DWEEDAR';
+        return ( < footer > < p > Done by { authorName } < /p></footer > )
+    }
+}
+export default Footer;
